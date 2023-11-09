94cfa8db9889c9ad03c4d2193db9a17aab3cddd5
camccaffrey <89548503+camccaffrey@users.noreply.github.com>
2023-11-09 17:34:13 -0500

    updated data_cleaning.R

--- Scripts/data_cleaning.R
+++ Scripts/data_cleaning.R
@@ -13,7 +13,10 @@ source(file.path("Scripts", "config.R"))
 library(tidyverse)
 
 # load data
-data <- read.csv(DATA_PATH)
+data <- read.csv(DATA_PATH) %>%
+  mutate(country = as.factor(country),
+         data_quality = as.factor(data_quality))
+
 data.dict <- read.csv(DICT_PATH)
 
 
@@ -30,41 +33,57 @@ test.data <- data[-sample.data, ]
 
 # Data Transformation Functions ------------------------------------------------
 
+# impute function
+impute <- function(df, medians) {
+  for (i in seq_along(medians)) {
+    df[, i][is.na(df[, i])] <- medians[i]
+  }
+  return(df)
+}
+
+# scale function
+normalize <- function(df, means, vars) {
+  for (i in seq_along(means)) {
+    df[, i] <- (df[, i]  - means[i]) / sqrt(vars[i])
+  }
+  return(df)
+}
+
 # get fit from training data
 fit <- function(df) {
-  # median x48
-  median.x48 <- median(df$x48, na.rm=TRUE)
   
-  # countries
-  country.table <- sort(table(df$country), decreasing=TRUE)
-  countries <- names(country.table[country.table > 50])[1]
-  country.missing <- "Other"
+  # get numeric columns
+  c <- ncol(df)
+  num.cols <- (1:c)[sapply(df, is.numeric)]
+  
+  
+  # mean, median, var
+  means <- sapply(df[num.cols], mean, na.rm=TRUE)
+  medians <- sapply(df[num.cols], median, na.rm=TRUE)
+  vars <- sapply(df[num.cols], var, na.rm=TRUE)
   
   # return list
-  list(median.x48=median.x48,
-       countries=countries,
-       country.missing=country.missing)
+  list(num.cols=num.cols, means=means, medians=medians, vars=vars)
 }
 
 # transform training or test data using fit
-transform <- function(df, fit_list) {
-  df.transform <- df %>%
-    # impute x48 value
-    mutate(x48 = ifelse(is.na(x48), fit_list$median.x48 + rnorm(1), x48)) %>%
-    
-    # mutate categorical variables
-    mutate(expensive = as.factor(ifelse(x48 > fit_list$median.x48, 1, 0)),
-           usa = as.factor(ifelse(country %in% fit_list$countries, 1, 0)),
-           data_quality = as.factor(data_quality))
+transform <- function(df, fit_list, impute=FALSE, scale=FALSE) {
+  # impute NAs
+  if (impute) {df[fit_list$num.cols] <- impute(df[fit_list$num.cols], fit_list$medians)}
   
+  # create expensive variable from x48
+  set.seed(SEED)
+  df$expensive = ifelse(df$x48 == fit_list$medians[48] | is.na(df$x48), rbinom(1, 1, 0.5), 0)
+  df$expensive = as.factor(ifelse(df$x48 < fit_list$medians[48] | is.na(df$x48), df$expensive, 1))
   
-  df.transform <- df.transform %>%
-    # rename columns with aliases
-    rename(all_of(setNames(colnames(df.transform), data.dict$Alias)))
+  # create USA variable
+  df$usa = as.factor(ifelse(df$country == "United States", 1, 0))
   
+  # scale numeric values
+  if (scale) {df[fit_list$num.cols] <- scale(df[fit_list$num.cols], fit_list$means, fit_list$vars)}
   
-  # return dataframe
-  df.transform
+  # rename columns
+  df %>% rename(all_of(setNames(colnames(df), data.dict$Alias)))
 }
 
 # pivot numeric columns to longer 
@@ -85,12 +104,12 @@ wide_to_long <- function(df) {
 my.fit <- fit(train.data)
 
 # transform datasets
-train.trans <- transform(train.data, my.fit)
-test.trans <- transform(test.data, my.fit)
+train.trans <- transform(train.data, my.fit, impute=TRUE, scale=FALSE)
+test.trans <- transform(test.data, my.fit, impute=TRUE, scale=FALSE)
+eda.trans <- transform(train.data, my.fit, impute=FALSE, scale=FALSE)
 
 # pivot longer (for EDA only)
-train.trans.long <- wide_to_long(train.trans)
-test.trans.long <- wide_to_long(train.trans)
+eda.long <- wide_to_long(eda.trans)
 
 
 # Save Transformed Data --------------------------------------------------------
@@ -98,6 +117,6 @@ test.trans.long <- wide_to_long(train.trans)
 saveRDS(train.trans, file=TRAIN_PATH)
 saveRDS(test.trans, file=TEST_PATH)
 
-saveRDS(train.trans.long, file=TRAINLONG_PATH)
-saveRDS(test.trans.long, file=TESTLONG_PATH)
+saveRDS(eda.trans, file=EDA_PATH)
+saveRDS(eda.long, file=EDALONG_PATH)
 
